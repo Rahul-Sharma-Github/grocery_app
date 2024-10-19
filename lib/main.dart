74da070da173e536b2e93f6f38268dc82231e145
Rahul Sharma <rahulsharma3258work@gmail.com>
2024-10-19 19:38:06 +0530

    added login screen Widget | and set it initial screen |

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:grocery_app/Features/Login/screens/login_screen.dart';
 
 void main() {
   runApp(
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Center(child: Text('Grocery App')),
+      home: const LoginScreen(),
     );
   }
 }
