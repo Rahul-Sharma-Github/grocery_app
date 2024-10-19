ce1e98a55a8ac985e49537f7592d0d7d3b6240d4
Rahul Sharma <rahulsharma3258work@gmail.com>
2024-10-19 21:08:39 +0530

    added images in assets folder | added GetX and google_fonts Pakage | changed MaterialApp to GetMaterialApp Widget | Created and added Colors for our App | Added Background + Logo + Title + Subtitle into login_screen |

--- lib/Features/Login/screens/login_screen.dart
+++ lib/Features/Login/screens/login_screen.dart
@@ -1,13 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:grocery_app/utils/colors.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  // Logo + Title + Subtitle
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Flexible(
+                        fit: FlexFit.loose,
+                        child: SizedBox(
+                          height: 30,
+                          width: 55,
+                          child: FittedBox(
+                            fit: BoxFit.cover,
+                            child: Image.asset('assets/logo.png'),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        fit: FlexFit.loose,
+                        child: SizedBox(
+                          child: FittedBox(
+                            fit: BoxFit.cover,
+                            child: Text(
+                              '!DealMart',
+                              style: GoogleFonts.roboto(
+                                fontSize: 32.0,
+                                fontWeight: FontWeight.bold,
+                                color: Color(AppColors.logoTextColor),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Flexible(
+                        fit: FlexFit.loose,
+                        child: Text(
+                          'Store',
+                          style: GoogleFonts.roboto(
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.w400,
+                            color: Color(AppColors.textColor),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+class AppColors {
+  static int loginButtonBackground = (0xff375f60);
+  static int textfieldIconColor = (0xffabb0b9);
+  static int textFieldInnerTextColor = (0xffa2a2a2);
+  static int textColor = (0xffffffff);
+  static int forgotPasswordTextColor = (0xff67b297);
+  static int logoTextColor = (0xff3a7f5a);
+}
